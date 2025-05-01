3c5595c71e073acd64929a561c12a393a7d79f2a
Ion Rusnac <ionrusnac@Ions-Laptop.local>
2025-05-01 13:58:13 +0300

    Edit Page Function

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -5,18 +5,18 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 import re
 from random import choice
-import markdown2
+from markdown2 import markdown
 
 class SearchForm(forms.Form):
     entry = forms.CharField(label = "Search")
+
 class EditPage(forms.Form):
     content = forms.CharField(label = "Edit Markdown content", widget=forms.Textarea())
+
 class CreatePage(forms.Form):
     title = forms.CharField(label = "Title")
-    content = forms.CharField(label = "Markdown content", widget=forms.Textarea(attrs={
-            'placeholder': 'Enter your content'
-        })
-    )
+    content = forms.CharField(label = "Markdown content", widget=forms.Textarea())
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "form": SearchForm(),
@@ -26,12 +26,15 @@ def index(request):
 def create(request):
     if request.method == "POST":
         form = CreatePage(request.POST)
+
         if form.is_valid():
             if form.cleaned_data["title"].lower() in util.list_entries():
+
                 return render(request, "encyclopedia/error.html", {
                     "form": SearchForm(),
                     "error": f"{form.cleaned_data['title']} entry already exists!"
                 })
+            
             else:
                 util.save_entry(form.cleaned_data["title"], form.cleaned_data["content"])
                 return HttpResponseRedirect(reverse("wiki:entry", args=[form.cleaned_data["title"]]))
@@ -44,16 +47,16 @@ def create(request):
         "create": CreatePage()
     })
 
-def edit(request):
+def edit(request, TITLE):
     if request.method == "POST":
         form = EditPage(request.POST)
         if form.is_valid():
-            title = request.POST.get('title', 'no')
-            with open(f"/entries/{title}.md", 'w') as file:
-                file.write(form.cleaned_data["content"])
+            util.save_entry(TITLE, form.cleaned_data["content"])
+            return HttpResponseRedirect(reverse("wiki:entry", args=[TITLE]))
     return render(request, "encyclopedia/edit.html",{
         "form": SearchForm(),
-        "edit": EditPage(initial={'content': 'XYZ'})
+        "title": TITLE.upper(),
+        "edit": EditPage(initial={'content': util.get_entry(TITLE)}),
     })
 
 def entry(request, TITLE):
@@ -62,22 +65,29 @@ def entry(request, TITLE):
     if request.method == "POST":
         form = SearchForm(request.POST)
         if form.is_valid():
+
             TITLE = form.cleaned_data["entry"]
+
             if TITLE.lower() in util.list_entries():
+
                 return HttpResponseRedirect(reverse("wiki:entry", args=[TITLE]))
+            
             else:
+
                 pattern = re.compile(rf"{TITLE}", re.IGNORECASE)
                 entries = util.list_entries(False)
                 entries_modified = []
+
                 for entry in entries:
                     if pattern.search(entry):
                         entries_modified.append(entry)
-                #entries = [entry for entry in util.list_entries() if TITLE.lower() in entry]
+
                 return render(request, "encyclopedia/results.html",{
                     "title": TITLE,
                     "entries": entries_modified,
                     "form": SearchForm()
                 })
+            
         else:
             return render(request, "encyclopedia/index.html", {
                 "form": form
@@ -89,6 +99,6 @@ def entry(request, TITLE):
         })
     return render(request, "encyclopedia/entry.html",{
         "title": TITLE.upper(),
-        "content": markdown2.markdown(util.get_entry(TITLE)),
+        "content": markdown(util.get_entry(TITLE), extras=["spoiler", "strike"]),
         "form": SearchForm()
     })
--- entries/CSS.md
+++ entries/CSS.md
@@ -1,3 +1,34 @@
 # CSS
 
-CSS is a language that can be used to add style to an [HTML](/wiki/HTML) page.
+~CSS~ is a language that can be used to add style to an [HTML](/wiki/HTML) page.
+
+
+> This is a x2 text
+
+Heading level 1
+====
+
+Heading level 2
+---
+
+[link](https://www.example.com/my%20great%20page)
+
+<a href="https://www.example.com/my great page">link</a>
+
+Love**is**bold
+
+> This is a blockquote
+
+Use `code` in your Markdown file.
+
+~~The world is flat.~~ We now know that the world is round.
+
+> Dorothy followed her through many of the beautiful rooms in her castle.
+
+ds
+
+
+
+
+
+ds
